06509994c47214cdcaba5dd5b7aa1b1ac19cb677
Christiaan Kruger <c@hristiaan.co.za>
2019-10-05 22:28:48 +0200

    Remove logs

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,16 +1,7 @@
 import { configure } from '@storybook/react';
-// import { setDefaults } from 'react-storybook-addon-props-combinations';
-// import 'typeface-open-sans';
-// import '~/index.scss';
-
-// import './storybook.scss';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../client', true, /\.stories\.tsx$/);
-
-// setDefaults({
-//   // overwrite global defaults here
-// });
 
 function loadStories() {
   req.keys().forEach(req);
